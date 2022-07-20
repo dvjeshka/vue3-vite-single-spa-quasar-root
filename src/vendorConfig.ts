6b6cb56d2233e5589391d02fd36d702a99b6f873
dvjeshko <r.bochkarev@bk.ru>
2022-07-20 12:03:53 +0300

    add depend

--- src/vendorConfig.ts
+++ src/vendorConfig.ts
@@ -7,6 +7,8 @@ interface IVendorItem {
 const vendorList = [
     { name:'vue', fileName:'vue.runtime.esm-browser.prod.js', nodeModulePath: 'node_modules/vue/dist/vue.runtime.esm-browser.prod.js' },
     { name:'vue-router', fileName:'vue-router.esm-browser.js', nodeModulePath: 'node_modules/vue-router/dist/vue-router.esm-browser.js' },
+    { name:'axios', fileName:'axios.js', nodeModulePath: 'node_modules/axios/dist/axios.js' },
+    { name:'@vueuse/integrations/useAxios', fileName:'@vueuse-integrations-useAxios.js', nodeModulePath: 'node_modules/@vueuse/integrations/useAxios.mjs' },
     { name:'quasar', fileName:'quasar.esm.prod.js', nodeModulePath: 'node_modules/quasar/dist/quasar.esm.prod.js' },
     { name:'quasar/lang/ru', fileName:'quasar-lang-ru.js', nodeModulePath: 'node_modules/quasar/lang/ru.js' },
     { name:'single-spa-vue', fileName:'single-spa-vue.js', nodeModulePath: 'node_modules/single-spa-vue/dist/esm/single-spa-vue.js' },
