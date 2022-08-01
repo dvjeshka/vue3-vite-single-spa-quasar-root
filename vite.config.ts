7761be910361fb568da9433c7387fb51c40a60cc
dvjeshko <r.bochkarev@bk.ru>
2022-08-01 15:24:53 +0300

    update

--- vite.config.ts
+++ vite.config.ts
@@ -13,69 +13,67 @@ import ImportMetaEnvPlugin from '@import-meta-env/unplugin';
 
 import { vendorConfig } from './src/vendorConfig';
 
-export default defineConfig(({ mode }) => {
-  return {
-    envPrefix: [], // https://iendeavor.github.io/import-meta-env/guide.html#framework-specific-notes
-    test: {
-      dir: './',
-      includeSource: ['./src/**/*.ts'],
-      environment: 'happy-dom',
-      coverage: {
-        src: './src',
-        /*  all: false,
-        lines: 80,
-        functions: 80,
-        branches: 80,
-        statements: 80,*/
-        reportsDirectory: '../test/utils/.coverage',
+export default defineConfig(() => ({
+  envPrefix: [], // https://iendeavor.github.io/import-meta-env/guide.html#framework-specific-notes
+  test: {
+    dir: './',
+    includeSource: ['./src/**/*.ts'],
+    environment: 'happy-dom',
+    coverage: {
+      src: ['./src'],
+      reportsDirectory: '../test/utils/.coverage',
+      /* all: true,
+       lines: 80,
+       functions: 80,
+       branches: 80,
+       statements: 80, */
+    },
+    reporters: ['vitest-sonar-reporter'],
+    outputFile: '../test-report.xml',
+  },
+  plugins: [
+    ImportMetaEnvPlugin.vite({
+      env: '.env.default',
+      example: '.env.runtime',
+    }),
+    ViteTips(),
+    Inspector(),
+    checker({ vueTsc: true }),
+    vue({ template: { transformAssetUrls } }),
+    quasar({ autoImportComponentCase: 'pascal' }),
+    compress({ verbose: true, brotli: false }),
+  ],
+  root: './src',
+  build: {
+    outDir: '../dist',
+    emptyOutDir: true,
+    target: 'esnext',
+    rollupOptions: {
+      preserveEntrySignatures: 'strict', // Оставляет exports у vendors
+      input: {
+        index: './src/index.html',
+        registerApplications: './src/registerApplications.ts',
+        ...vendorConfig.input,
       },
-      reporters: 'vitest-sonar-reporter',
-      outputFile: '../test-report.xml',
-    },
-    plugins: [
-      ImportMetaEnvPlugin.vite({
-        env: '.env.default',
-        example: '.env.runtime',
-      }),
-      ViteTips(),
-      Inspector(),
-      checker({ vueTsc: true }),
-      vue({ template: { transformAssetUrls } }),
-      quasar({ autoImportComponentCase: 'pascal' }),
-      compress({ verbose: true, brotli: false }),
-    ],
-    root: './src',
-    build: {
-      outDir: '../dist',
-      emptyOutDir: true,
-      target: 'esnext',
-      rollupOptions: {
-        preserveEntrySignatures: true,
-        input: {
-          index: './src/index.html',
-          registerApplications: './src/registerApplications.ts',
-          ...vendorConfig.input,
-        },
-        output: {
-          entryFileNames: vendorConfig.output.entryFileNames,
-        },
-        external: vendorConfig.external,
+      output: {
+        entryFileNames: vendorConfig.output.entryFileNames,
       },
+      external: vendorConfig.external,
     },
-    define: {
-      'import.meta.vitest': false,
+  },
+  define: {
+    'import.meta.vitest': false,
+  },
+  resolve: {
+    alias: {
+      '@': resolve(__dirname, 'src'),
     },
-    resolve: {
-      alias: {
-        '@': resolve(__dirname, 'src'),
-      },
-    },
-    server: {
-      port: 9000,
-      open: true,
-    },
-    preview: {
-      port: 9001,
-    },
-  };
-});
+  },
+  server: {
+    port: 9000,
+    open: true,
+  },
+  preview: {
+    port: 9001,
+  },
+}));
